5db9e464be48571a3532b9b330da413caa889217
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-12-25 19:07:58 +0100

    better app

--- src/main.c
+++ src/main.c
@@ -4,7 +4,11 @@
 #include "dht22.h"
 #include "lcd1602.h"
 
-#define DEGREE_CHAR 223 // char code for degree symbol on lcd1602
+// char code for degree symbol on lcd1602
+const char DEGREE_CHAR = 223;
+
+// measurement interval in us
+const uint32_t MEASUREMENT_INTERVAL = 20 * 1000 * 1000; // 20s
 
 // pinout
 const uint LED_PIN = 25;
@@ -34,22 +38,27 @@ enum { CURRENT, MINMAX } display_mode = CURRENT;
 struct {
     int16_t current_temp, min_temp, max_temp;
     uint16_t current_hum, min_hum, max_hum;
-} data = { 0, 32767, -32768, 0, 65535, 0 };
+} data = {0};
 
 // switch timestamp in us for debounce and longpress detection
 uint32_t sw_pressed_since = 0;
+
+// last measurement timestamp in us
+uint32_t last_measurement = 0;
 
 // whether display needs update
 bool display_needs_update = true;
 
 // update data with new measurement
-void update_data(int16_t temp, int16_t hum) {
+bool update_data(int16_t temp, uint16_t hum) {
+    if (temp == data.current_temp && hum == data.current_hum) return false; // no change
     data.current_temp = temp;
     if (temp < data.min_temp) data.min_temp = temp;
     if (temp > data.max_temp) data.max_temp = temp;
     data.current_hum = hum;
     if (hum < data.min_hum) data.min_hum = hum;
     if (hum > data.max_hum) data.max_hum = hum;
+    return true;
 }
 
 // update display according to current mode
@@ -98,6 +107,7 @@ int main() {
     // status led
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
 
     // dht config
     dht22_t dht;
@@ -106,6 +116,9 @@ int main() {
     // lcd config
     lcd1602_init(&lcd);
     lcd1602_set_display_on(&lcd, true, false, false);
+    lcd1602_write_string(&lcd, "Hi there! :)");
+    lcd1602_set_cursor_pos(&lcd, 1, 0);
+    lcd1602_write_string(&lcd, "Switch: Min-Max");
 
     // switch config
     gpio_init(SW_PIN);
@@ -113,13 +126,17 @@ int main() {
     gpio_pull_up(SW_PIN);
     gpio_set_irq_enabled_with_callback(SW_PIN, GPIO_IRQ_EDGE_FALL, true, switch_pressed);
 
-    // trigger initial measurement
+    // initial measurement
     dht22_trigger_measurement(&dht);
+    last_measurement = time_us_32();
+    while (!dht22_is_ready(&dht) && !dht22_has_error(&dht)) dht22_task(&dht);
+    data.min_temp = data.max_temp = data.current_temp = dht22_get_temperature(&dht);
+    data.min_hum = data.max_hum = data.current_hum = dht22_get_humidity(&dht);
 
     // runloop
     while (1) {
         // status led
-        gpio_put(LED_PIN, dht22_is_ready(&dht));
+        gpio_put(LED_PIN, !dht22_is_ready(&dht));
 
         // clear min-max values on longpress (1s)
         if (gpio_get(SW_PIN) == 0 && time_us_32() - sw_pressed_since > 1000000) {
@@ -129,13 +146,21 @@ int main() {
             sw_pressed_since = time_us_32();
         }
 
+        // trigger measurement
+        if (time_us_32() - last_measurement > MEASUREMENT_INTERVAL) {
+            dht22_trigger_measurement(&dht);
+            last_measurement = time_us_32();
+        }
+
         // update data once ready
         if (dht22_is_ready(&dht)) {
-            update_data(dht22_get_temperature(&dht), dht22_get_humidity(&dht));
-            display_needs_update = true;
-            dht22_trigger_measurement(&dht); // retrigger
-        } else if (dht22_has_error(&dht)) {
-            dht22_reset(&dht); // reset error
+            bool got_new_data = update_data(dht22_get_temperature(&dht), dht22_get_humidity(&dht));
+            if (got_new_data) display_needs_update = true;
+        }
+
+        // reset error
+        if (dht22_has_error(&dht)) {
+            dht22_reset(&dht);
         }
 
         // display needs update
